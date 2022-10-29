49793988e53174e3cf71f676636dca2a0d871311
Ionut2003 <ionutparaschiv360@gmail.com>
2022-10-29 00:17:33 -0700

    pizza buton

--- functions.js
+++ functions.js
@@ -1,3 +1,13 @@
-const btn = document.querySelectorAll(".button");
-const pgn = document.querySelectorAll(".pagina");
-
+const buttons = document.getElementsByClassName('button');
+var globalVar;
+buttons[0].addEventListener('click', (e) =>{
+    const img = document.createElement("img");
+    img.src = "/pizza.gif"
+    img.width = "100";
+    img.height = "100";
+    document.getElementById('pizza').appendChild(img);
+    globalVar = img;
+})
+buttons[1].addEventListener('click', (e) =>{
+    globalVar.remove();
+})
