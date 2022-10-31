b4b34c08bbb4d613a98b2414ae9e5254ae4ecc51
Ionut2003 <ionutparaschiv360@gmail.com>
2022-10-31 00:47:00 -0700

    pizza images load page

--- functions.js
+++ functions.js
@@ -1,13 +1,7 @@
 const buttons = document.getElementsByClassName('button');
 var globalVar;
 buttons[0].addEventListener('click', (e) =>{
-    const img = document.createElement("img");
-    img.src = "/pizza.gif"
-    img.width = "100";
-    img.height = "100";
-    document.getElementById('pizza').appendChild(img);
-    globalVar = img;
+    window.location.href = "pizza.html";  
+
 })
-buttons[1].addEventListener('click', (e) =>{
-    globalVar.remove();
-})
+console.log(document.getElementsByName("pizza").clientWidth);
