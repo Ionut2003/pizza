2b32bc3e50d48156e828e963e183f56edb91a9bb
mas-vi <opreaionut035@gmail.com>
2022-11-16 18:49:10 +0200

    Reorganizat structura cu Ion

--- functions.js
+++ functions.js
@@ -1,26 +1,32 @@
-const buttons = document.getElementsByClassName('button');
-
+const buttons = document.getElementsByClassName("button");
+const telefon = document.getElementsByClassName("nr-telefon");
 let firstLoad = true;
+
+let nrTelefon = "2222.222.222";
 
 if(firstLoad)
 {
-    $("#view").load("pizza.html");
+    $("#view").load("pagini/pizza.html");
     firstLoad = false;
 }
 
 buttons[0].addEventListener('click', (e) =>{
-    $("#view").load("pizza.html"); 
+    $("#view").load("pagini/pizza.html"); 
 })
         
 buttons[1].addEventListener('click', (e) =>{
-    $("#view").load("paste.html");    
+    $("#view").load("pagini/paste.html");    
 })
 buttons[2].addEventListener('click', (e) =>{
-    $("#view").load("salate.html");     
+    $("#view").load("pagini/salate.html");     
 })
 buttons[3].addEventListener('click', (e) =>{
-    $("#view").load("desert.html");     
+    $("#view").load("pagini/desert.html");     
 })
 buttons[4].addEventListener('click', (e) =>{
-    $("#view").load("bauturi.html");    
+    $("#view").load("pagini/bauturi.html");    
+})
+
+telefon[0].addEventListener('click',(e)=>{
+    navigator.clipboard.writeText(nrTelefon)
 })
