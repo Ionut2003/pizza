72453c689246c24672adab71a41f12c65f66b504
mas-vi <opreaionut035@gmail.com>
2022-11-18 19:22:12 +0200

    Creat partial Cosul de cumparaturi

--- functions.js
+++ functions.js
@@ -5,36 +5,77 @@ const reclamatii = document.getElementsByClassName("reclamatii");
 const contact = document.getElementsByClassName("contact");
 const feReclamatii = document.getElementById("fereastraReclamatii");
 const modal = document.getElementById("modal");
-
+const butonCos =document.getElementById("cos-cumparaturi-icon");
+const cosCumparaturi = document.getElementById("cos-cumparaturi");
+const inchideCos = document.getElementById("inchide-cos");
+const view = document.getElementById("view");
+let btns={};
 let firstLoad = true;
 let textCopied = false;
 let nrTelefon = "2222.222.222";
+
 
 
 if(firstLoad)
 {
     $("#view").load("pagini/pizza.html");
     firstLoad = false;
+    btns = document.getElementsByClassName("container");
+    
+    
 }
+
+
+
+
+
+
+
+
+butonCos.addEventListener('click',(e)=>{
+    cosCumparaturi.style.display="flex";
+    butonCos.style.display="none";
+    inchideCos.style.display="flex";
+})
+
+inchideCos.addEventListener('click',(e)=>{
+    cosCumparaturi.style.display="none";
+    butonCos.style.display="flex";
+    inchideCos.style.display="none";
+})
+
+
 
 buttons[0].addEventListener('click', (e) =>{
     $("#view").load("pagini/pizza.html"); 
+    btns = document.getElementsByClassName("container");
+    
+
 })
         
 buttons[1].addEventListener('click', (e) =>{
-    $("#view").load("pagini/paste.html");    
+    $("#view").load("pagini/paste.html"); 
+    btns = document.getElementsByClassName("container");
+    
+
 })
 buttons[2].addEventListener('click', (e) =>{
-    $("#view").load("pagini/salate.html");     
+    $("#view").load("pagini/salate.html");  
+    btns = document.getElementsByClassName("container");
+    
 })
 buttons[3].addEventListener('click', (e) =>{
-    $("#view").load("pagini/desert.html");     
+    $("#view").load("pagini/desert.html");  
+    btns = document.getElementsByClassName("container");
+    
 })
 buttons[4].addEventListener('click', (e) =>{
-    $("#view").load("pagini/bauturi.html");    
+    $("#view").load("pagini/bauturi.html");  
+    btns = document.getElementsByClassName("container");
+    
 })
 promotii[0].addEventListener('click', (e) =>{
-    $("#view").load("pagini/promotii.html");    
+    $("#view").load("pagini/promotii.html");   
 })
 reclamatii[0].addEventListener('click', (e) =>{
     setTimeout(function() {
@@ -43,6 +84,8 @@ reclamatii[0].addEventListener('click', (e) =>{
     
 
 })
+
+
 
 telefon.addEventListener('click',(e)=>{
     navigator.clipboard.writeText(nrTelefon);
@@ -70,3 +113,18 @@ window.onclick = function(event){
         modal.style.display="none";
     }
 }
+
+window.onclick = function(event){
+    for(let i=0;i<btns.length;i++)
+    {
+        let btn = btns[i];
+        btn.addEventListener('click',(e)=>{
+            let img = btn.children[0];
+            console.log(img.src);
+        
+        })
+
+    }
+}
+
+
