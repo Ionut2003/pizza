015048a7d2c05bbcb6d3c83939fd59d6b00fd8a7
Ionut Paraschiv <ionutparaschiv360@gmail.com>
2024-04-06 14:44:26 +0300

    more changes applied and a little bit of mobile modify

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 const buttons = document.getElementsByClassName("button");
 const telefon = document.getElementById("nr-telefon");
-const promotii = document.getElementsByClassName("primul-mesaj");
 const reclamatii = document.getElementsByClassName("reclamatii");
 const contact = document.getElementsByClassName("contact");
 const feReclamatii = document.getElementById("fereastraReclamatii");
@@ -9,13 +8,17 @@ const butonCos =document.getElementById("cos-cumparaturi-icon");
 const cosCumparaturi = document.getElementById("cos-cumparaturi");
 const inchideCos = document.getElementById("inchide-cos");
 const view = document.getElementById("view");
-const mediaQuery = window.matchMedia('(max-width: 600px)');
 const comanda = document.getElementById("comanda");
-let btns={};
 let firstLoad = true;
 let textCopied = false;
 let nrTelefon = "222.222.2222";
 let valoareComanda = 0;
+let produseInCos = [];
+let mediaQuery = window.matchMedia("(max-width: 600px)");
+const navbar1 = document.getElementsByClassName("navbar1")[0];
+const navbar2 = document.getElementsByClassName("navbar2")[0];
+
+$("#view").load("./pagini/pizza.html");
 
 class Produs{
     nume={};
@@ -27,72 +30,9 @@ class Produs{
         this.pret = pret;
         this.src=src;
     }
-
-
 }
 
 
-
-
-
-let produseInCos = [];
-
-if (mediaQuery.matches) {
-    const navbar1 = document.getElementsByClassName("navbar1");
-    const navbar2 = document.getElementsByClassName("navbar2");
-    let nav1_touched = false;
-    let nav2_touched = false;
-    navbar1[0].addEventListener('touchstart', (e) =>{
-        if(nav1_touched == false){
-            if (nav2_touched == true){
-                document.getElementsByClassName("tipuri-meniuri")[0].style.display = "none";
-            }
-            document.getElementById("backGround").style.display = "flex";
-            document.getElementById("backGround").style.backgroundColor = "#049a86";
-            document.getElementById("backGround").style.transform = "translate(-100%, 0)";
-            navbar1[0].style.transform = "translate(-300%, 0)";
-            navbar2[0].style.transform = "translate(-300%, 0)";
-            document.getElementsByClassName("reclame")[0].style.display = "inline-block";
-            nav1_touched = true;
-        }
-        else{
-            navbar1[0].style.transform = "translate(0%)";
-            navbar2[0].style.transform = "translate(0%)";
-            document.getElementById("backGround").style.display = "none";
-            document.getElementsByClassName("reclame")[0].style.display = "none";
-            nav1_touched = false;
-        }
-    });
-    navbar2[0].addEventListener('touchstart', (e) =>{
-        if(nav2_touched == false){
-            if (nav1_touched == true){
-                document.getElementsByClassName("reclame")[0].style.display = "none";
-            }
-            document.getElementById("backGround").style.display = "flex";
-            document.getElementById("backGround").style.backgroundColor = "#d04f4c";
-            document.getElementById("backGround").style.transform = "translate(-100%, 0)";
-            navbar1[0].style.transform = "translate(-300%, 0)";
-            navbar2[0].style.transform = "translate(-300%, 0)";
-            document.getElementsByClassName("tipuri-meniuri")[0].style.display = "inline-block";
-            nav2_touched = true;
-        }
-        else{
-            navbar1[0].style.transform = "translate(0%)";
-            navbar2[0].style.transform = "translate(0%)";
-            document.getElementById("backGround").style.display = "none";
-            document.getElementsByClassName("tipuri-meniuri")[0].style.display = "none";
-            nav2_touched = false;
-        }
-    });
-}
-
-
-if(firstLoad)
-{
-    $("#view").load("./pagini/pizza.html");
-    firstLoad = false;
-    btns = document.getElementsByClassName("container");
-}
 
 
 
@@ -119,127 +59,135 @@ comanda.addEventListener('click',(e)=>{
     comanda.style.display="none";
 })
 
+// NAVBAR 2 EVENTS
 
 
 buttons[0].addEventListener('click', (e) =>{
     $("#view").load("./pagini/pizza.html"); 
-    btns = document.getElementsByClassName("container");
     
 
 })
         
 buttons[1].addEventListener('click', (e) =>{
     $("#view").load("./pagini/paste.html"); 
-    btns = document.getElementsByClassName("container");
     
 
 })
 buttons[2].addEventListener('click', (e) =>{
     $("#view").load("./pagini/salate.html");  
-    btns = document.getElementsByClassName("container");
     
 })
 buttons[3].addEventListener('click', (e) =>{
     $("#view").load("./pagini/desert.html");  
-    btns = document.getElementsByClassName("container");
     
 })
 buttons[4].addEventListener('click', (e) =>{
     $("#view").load("./pagini/bauturi.html");  
-    btns = document.getElementsByClassName("container");
     
 })
-promotii[0].addEventListener('click', (e) =>{
-    $("#view").load("./pagini/promotii.html");
-    btns = document.getElementsByClassName("container");   
-})
-reclamatii[0].addEventListener('click', (e) =>{
-    setTimeout(function() {
-        modal.style.display="flex";
-      }, 100);
-    
 
-})
+// MOBILE VERSION 
 
+if(mediaQuery.matches) {
+    document.getElementById("menu_mobile").appendChild(document.getElementsByClassName("navbar1")[0]);
+    document.getElementById("menu_mobile").appendChild(document.getElementsByClassName("navbar2")[0]);
 
-
-telefon.addEventListener('click',(e)=>{
-    navigator.clipboard.writeText(nrTelefon);
-    if(textCopied==false){
-        let clipC = document.createElement("div");
-        clipC.setAttribute("id", "clipboardNr");
-        clipC.innerHTML = "Copiat";
-        document.body.appendChild(clipC);
-        clipC.style.left = event.clientX + 10 + 'px';
-        clipC.style.top = event.clientY + 10 + 'px';
-        textCopied = true;
-        let delayInMilliseconds = 500; 
-        setTimeout(function() {
-          clipC.parentNode.removeChild(clipC);
-          textCopied = false;
-        }, delayInMilliseconds);
-    }
-})
-
-window.onclick = function(event){
-    if(event.target !=feReclamatii && event.target != document.getElementById("fname")
-      && event.target != document.getElementById("email")
-      && event.target != document.getElementById("mesaj"))
-    {
-        modal.style.display="none";
-    }
-    for(let i=0;i<btns.length;i++)
-    {
+    // CLICK MENU EVENT 
+    document.getElementById("menu_mobile").addEventListener("click", function(event) {
+        for(let i = 0; i < 3; i++) { //making the lines disapear
+            document.getElementsByClassName("line")[i].style.display = "none";
+        }
+        document.getElementById("menu_mobile").style.width = "auto";
+        document.getElementById("menu_mobile").style.height = "auto";
+        navbar1.style.display = "flex";
+        navbar2.style.display = "flex";
         
-        let btn = btns[i];
-        btn.addEventListener('click',(e)=>{
-            
-                let img = btn.children[0];
-                let nume = btn.children[1].innerHTML;
-                let pret = parseInt(btn.children[2].innerHTML);
-                let fullPath = img.src;
-                let pos = fullPath.indexOf("poze");
-                let partPath = "./" + fullPath.slice(pos)
-                let produs = new Produs(nume,pret,partPath);
-                let shouldAppend = true;
-                produseInCos.forEach(function(prod){
-                    if(produs.nume === prod.nume)
-                    shouldAppend = false;
-                })
+    });
+}
 
-                if(shouldAppend)
-                {
-                    produseInCos.push(produs);
-                    let cartItem = document.createElement('div');
-                    cartItem.classList.add("produs");
-                    cartItem = `
+
+
+
+
+// promotii[0].addEventListener('click', (e) =>{
+//     $("#view").load("./pagini/promotii.html");   
+// })
+
+
+
+// reclamatii[0].addEventListener('click', (e) =>{
+//     setTimeout(function() {
+//         modal.style.display="flex";
+//       }, 100);
+    
+
+// })
+
+
+// NR-TELEFON COPIED EVENT 
+
+telefon.addEventListener('click',(e)=>{ 
+    navigator.clipboard.writeText(nrTelefon);
+})
+
+// window.onclick = function(event){
+//     if(event.target !=feReclamatii && event.target != document.getElementById("fname")
+//       && event.target != document.getElementById("email")
+//       && event.target != document.getElementById("mesaj"))
+//     {
+//         modal.style.display="none";
+//     }
+    // for(let i=0;i<btns.length;i++)
+    // {
+        
+    //     let btn = btns[i];
+    //     btn.addEventListener('click',(e)=>{
+            
+    //             let img = btn.children[0];
+    //             let nume = btn.children[1].innerHTML;
+    //             let pret = parseInt(btn.children[2].innerHTML);
+    //             let fullPath = img.src;
+    //             let pos = fullPath.indexOf("poze");
+    //             let partPath = "./" + fullPath.slice(pos)
+    //             let produs = new Produs(nume,pret,partPath);
+    //             let shouldAppend = true;
+    //             produseInCos.forEach(function(prod){
+    //                 if(produs.nume === prod.nume)
+    //                 shouldAppend = false;
+    //             })
+
+    //             if(shouldAppend)
+    //             {
+    //                 produseInCos.push(produs);
+    //                 let cartItem = document.createElement('div');
+    //                 cartItem.classList.add("produs");
+    //                 cartItem = `
                     
-                    <div class="produs">
-                    <img src="${produs.src}" class="img"> 
-                    <div>
-                    ${produs.nume}
-                    </div>
-                    <div>     
-                    ${produs.pret}
-                    </div>
-                    </div>
+    //                 <div class="produs">
+    //                 <img src="${produs.src}" class="img"> 
+    //                 <div>
+    //                 ${produs.nume}
+    //                 </div>
+    //                 <div>     
+    //                 ${produs.pret}
+    //                 </div>
+    //                 </div>
                     
-                    `;
-                    $("#cos-cumparaturi").append(cartItem);
+    //                 `;
+    //                 $("#cos-cumparaturi").append(cartItem);
                     
                     
-                }
+    //             }
                 
              
                       
-        })
+    //     })
 
              
         
-    }
-    
-    console.log(produseInCos);
-}
+    // }
+
+// }
 
 
 
